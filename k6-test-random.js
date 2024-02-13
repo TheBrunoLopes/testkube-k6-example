359897ed8daee2130af2679eda4614d2b53f395d
Bruno Lopes <brunoleonelopes@gmail.com>
2024-02-13 15:37:19 +0400

    first commit

---
+++ k6-test-random.js
@@ -0,0 +1,19 @@
+// This k6 test was made to fail randomly 50% of the times.
+import http from 'k6/http';
+import { check, fail, sleep } from 'k6';
+
+
+export const options = {
+ stages: [
+   { duration: '1s', target: 1 },
+ ],
+};
+
+let statusCode = Math.random() > 0.5 ? 200 : 502;
+export default function () {
+ const res = http.get('https://httpbin.test.k6.io/');
+ check(res, { 'Check if status code is 200': (r) => { 
+    console.log(statusCode, "Passing? ", 200 == statusCode);
+    return r.status == statusCode }
+});
+}
